02ecd68317846ff07a721d940c21820e345f6fd3
Kyunghoon Kim <65442366+kkh0920@users.noreply.github.com>
2023-10-28 11:22:25 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a, b, c, i = 0;
+	
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if(b >= c){
+		printf("-1\n");
+	}
+	
+	else{
+		i = a / (c - b) + 1;
+		printf("%d\n", i);
+	}
+	
+	return 0;
+}
