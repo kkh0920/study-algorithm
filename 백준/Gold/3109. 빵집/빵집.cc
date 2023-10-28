b6f42df8d5bd4dce2d10b04cde7130ee1091a5e4
Kyunghoon Kim <65442366+kkh0920@users.noreply.github.com>
2023-10-28 11:36:35 +0900

    [Gold II] Title: 빵집, Time: 96 ms, Memory: 6912 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int R, C, result = 0;
+int dx[3] = { -1, 0, 1 }, dy[3] = { 1, 1, 1 };
+char arr[10000][501];
+
+bool dfs(int curI, int curJ){
+    if(curJ == C - 1){
+        result++;
+        return true;
+    }
+
+    for(int i = 0; i < 3; i++){
+        int nI = curI + dx[i];
+        int nJ = curJ + dy[i];
+        if(nI < 0 || nI >= R)
+            continue;
+        if(arr[nI][nJ] == 'x')
+            continue;
+
+        arr[nI][nJ] = 'x';
+
+        if(dfs(nI, nJ))
+            return true;
+    }
+
+    return false;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> R >> C;
+
+    for(int i = 0; i < R; i++){
+        for(int j = 0; j < C; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < R; i++){
+        dfs(i, 0);
+    }
+
+    cout << result << '\n';
+}
