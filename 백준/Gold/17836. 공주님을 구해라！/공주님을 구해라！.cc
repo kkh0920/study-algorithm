3878baba0fa4cd6dc582d4cba0228092ab7a1cf2
Kyunghoon Kim <65442366+kkh0920@users.noreply.github.com>
2024-08-11 17:50:36 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int n, m, t, map[100][100];
+bool visited[2][100][100];
+int dx[4] = { -1, 1, 0, 0 }, dy[4] = { 0, 0, -1, 1 };
+
+int bfs() {
+    queue< pair<bool, pair<int, int> > > q;
+    q.push(make_pair(false, make_pair(0, 0)));
+    visited[0][0][0] = true;
+    int time = 0;
+    while(!q.empty()) {
+        int size = q.size();
+        while(size--) {
+            bool sword = q.front().first;
+            int curI = q.front().second.first;
+            int curJ = q.front().second.second;
+            q.pop();
+
+            if(curI == n - 1 && curJ == m - 1)
+                return time;
+
+            for(int i = 0; i < 4; i++) {
+                bool nSword;
+                int nI = curI + dx[i];
+                int nJ = curJ + dy[i];
+                if(nI < 0 || nI >= n || nJ < 0 || nJ >= m)
+                    continue;
+                if(!sword && map[nI][nJ] == 1)
+                    continue;
+                nSword = map[nI][nJ] == 2 ? true : sword;
+                if(visited[nSword][nI][nJ])
+                    continue;
+                visited[nSword][nI][nJ] = true;
+                q.push(make_pair(nSword, make_pair(nI, nJ)));
+            }
+        }
+        if(++time > t) return -1;
+    }
+    return -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m >> t;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    int result = bfs();
+    if(result == -1) {
+        cout << "Fail" << '\n'; 
+    } else {
+        cout << result << '\n';
+    }
+
+    return 0;
+}
