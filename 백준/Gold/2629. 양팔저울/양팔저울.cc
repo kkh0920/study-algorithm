dd7e0fa4394915d8ea21a2f2cfb065f2816ce908
Kyunghoon Kim <65442366+kkh0920@users.noreply.github.com>
2024-09-14 13:07:43 +0900

    [Gold III] Title: 양팔저울, Time: 0 ms, Memory: 10500 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int W, B;
+vector<int> weight;
+int dp[70010][31];
+
+int solve(int bw, int index) {
+    if(dp[bw + 15000][index] != 0) return dp[bw + 15000][index];
+    if(bw == 0) return 1;
+    for(int i = index; i < W; i++) {
+        if(solve(bw - weight[i], i + 1) == 1) 
+            return dp[bw + 15000][index] = 1;
+        if(solve(bw + weight[i], i + 1) == 1)
+            return dp[bw + 15000][index] = 1;
+    }
+    return dp[bw + 15000][index] = -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> W;
+
+    int w;
+    for(int i = 0; i < W; i++) {
+        cin >> w;
+        weight.push_back(w);
+    }
+
+    cin >> B;
+
+    int bw;
+    for(int i = 0; i < B; i++) {
+        cin >> bw;
+        cout << (solve(bw, 0) == 1 ? "Y" : "N") << '\n';
+    }
+
+    return 0;
+}
