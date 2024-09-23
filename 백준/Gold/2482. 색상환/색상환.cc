c9edebc076c7622a2274b6524903b92e6519582c
Kyunghoon Kim <65442366+kkh0920@users.noreply.github.com>
2024-09-23 13:55:27 +0900

    [Gold III] Title: 색상환, Time: 12 ms, Memory: 9848 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.cc
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int N, K, dp[2][1001][1001], s;
+int N, K, dp[2][1001][1001];
 
-int solve(int index, int k) {
-    if(index == N - 1 && s == 0) return 0;
+int solve(int start, int index, int k) {
+    if(k == 0) return 1;
     if(index >= N) return 0;
 
-    if(dp[s][index][k] != -1) return dp[s][index][k];
-    if(k == 0) return 1;
+    if(dp[start][index][k] != -1) return dp[start][index][k];
     
     int result = 0;
-    for(int i = index + 2; i < N; i++) {
-        result += solve(i, k - 1);
-        result %= 1000000003;
+    if(!(index == N - 1 && start == 0)) {
+        result += solve(start, index + 2, k - 1);
     }
+    result += solve(start, index + 1, k);
 
-    return dp[s][index][k] = result;
+    return dp[start][index][k] = result % 1000000003;
 }
 
 int main() {
@@ -26,20 +25,13 @@ int main() {
     cin >> N >> K;
 
     for(int i = 0; i < N; i++) {
-        for(int j = 0; j < K; j++) {
+        for(int j = 0; j <= K; j++) {
             dp[0][i][j] = -1;
             dp[1][i][j] = -1;
         }
     }
 
-    int result = 0;
-    for(int i = 0; i < N; i++) {
-        s = (i > 0);
-        result += solve(i, K - 1);
-        result %= 1000000003;
-    }
-
-    cout << result << '\n';
+    cout << (solve(0, 2, K - 1) + solve(1, 1, K)) % 1000000003 << '\n';
 
     return 0;
 }
